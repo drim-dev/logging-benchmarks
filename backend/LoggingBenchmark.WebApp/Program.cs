e2924ee92a53e5332b403889fdfb61f1b99bc38e
Sergey Rubtsov <rubtsov.sergey.v@gmail.com>
2024-12-20 23:57:49 +0200

    [Backend]: Postgres. Draft

--- backend/LoggingBenchmark.WebApp/Program.cs
+++ backend/LoggingBenchmark.WebApp/Program.cs
@@ -1,5 +1,7 @@
 using IdGen.DependencyInjection;
+using LoggingBenchmark.Resources;
 using LoggingBenchmark.WebApp;
+using LoggingBenchmark.WebApp.Database;
 using LoggingBenchmark.WebApp.Features.Projects;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,6 +9,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.AddLogging();
 
 builder.Services.AddOpenApi();
+
+builder.AddNpgsqlDbContext<WebAppDbContext>(ResourceName.WebAppDb);
 
 builder.Services.AddIdGen(1);
 
